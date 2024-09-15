7f50b727926f11329cf7acb14429ff617f3c451a
Bart van Nierop <mail@bvnierop.nl>
2024-09-15 20:16:58 +0200

    Split view between left/right part

---
+++ src/AiNotes/Views/NoteDetailsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AiNotes.Views;
+
+public partial class NoteDetailsView : UserControl
+{
+    public NoteDetailsView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/AiNotes/Views/NoteListView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AiNotes.Views;
+
+public partial class NoteListView : UserControl
+{
+    public NoteListView()
+    {
+        InitializeComponent();
+    }
+}
