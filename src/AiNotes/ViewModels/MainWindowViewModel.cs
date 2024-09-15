c9e65b9ff79f1405375e7bfae961d769d12f8c7b
Bart van Nierop <mail@bvnierop.nl>
2024-09-15 19:18:49 +0200

    Make title editable

--- src/AiNotes/ViewModels/MainWindowViewModel.cs
+++ src/AiNotes/ViewModels/MainWindowViewModel.cs
@@ -6,23 +6,23 @@ namespace AiNotes.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    private Note _selectedNote;
-    public Note SelectedNote
+    private NoteViewModel _selectedNote;
+    public NoteViewModel SelectedNote
     {
         get => _selectedNote;
         set => this.RaiseAndSetIfChanged(ref _selectedNote, value);
     }
 
-    public ObservableCollection<Note> Notes { get; set; }
+    public ObservableCollection<NoteViewModel> Notes { get; set; }
 
     // Constructor
     public MainWindowViewModel()
     {
-        Notes = new ObservableCollection<Note>
+        Notes = new ObservableCollection<NoteViewModel>
         {
-            new Note("Note 1", "Content for Note 1"),
-            new Note("Note 2", "Content for Note 2"),
-            new Note("Note 3", "Content for Note 3")
+            new(new Note("Note 1", "Content for Note 1")),
+            new(new Note("Note 2", "Content for Note 2")),
+            new(new Note("Note 3", "Content for Note 3"))
         };
 
         // Set a default selection (first note)
--- src/AiNotes/ViewModels/NoteViewModel.cs
+++ src/AiNotes/ViewModels/NoteViewModel.cs
@@ -1,10 +1,32 @@
 using System.Collections.Generic;
+using AiNotes.Models;
+using ReactiveUI;
 
 namespace AiNotes.ViewModels;
 
-public class NoteViewModel : ViewModelBase
+public class NoteViewModel(Note note) : ViewModelBase
 {
-    public string Title { get; set; } = string.Empty;
-    public string Body { get; set; } = string.Empty;
-    public List<string> Attachments { get; set; } = new();
+    private Note _note = note;
+
+    // Title property with change notification
+    public string Title
+    {
+        get => _note.Title;
+        set {
+            var t = _note.Title;
+            this.RaiseAndSetIfChanged(ref t, value);
+            _note.Title = t;
+        }
+    }
+
+    // Body property with change notification
+    public string Body
+    {
+        get => _note.Body;
+        set {
+            var t = _note.Body;
+            this.RaiseAndSetIfChanged(ref t, value);
+            _note.Body = t;
+        }
+    }
 }
