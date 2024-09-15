e2ab4551d47d5ae02a8b6c6cdd1f34a7efe28b45
Bart van Nierop <mail@bvnierop.nl>
2024-09-15 17:39:52 +0200

    Add initial view

--- src/AiNotes/ViewModels/MainWindowViewModel.cs
+++ src/AiNotes/ViewModels/MainWindowViewModel.cs
@@ -1,20 +1,53 @@
-﻿namespace AiNotes.ViewModels;
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
+
+namespace AiNotes.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => """
-                              # Heading1
+    private string _selectedNoteTitle;
+    private string _selectedNoteBody;
 
-                              Hello Markdown.Avalonia!
+    public ObservableCollection<string> NoteTitles { get; set; }
 
-                              * listitem1
-                              * listitem2
+    // Property for the currently selected note title
+    public string SelectedNoteTitle
+    {
+        get => _selectedNoteTitle;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _selectedNoteTitle, value);
+            // When a new note is selected, update the content
+            if (value != null)
+            {
+                // Simulate loading content (in a real scenario, load from a data source)
+                SelectedNoteBody = $"This is the content of '{value}'";
+            }
+        }
+    }
 
-                              | col1 | col2 | col3 |
-                              |------|------|------|
-                              | one  |------|------|
-                              | two  |------|------|                         
-                              """;
+    // Property for the selected note's content
+    public string SelectedNoteBody
+    {
+        get => _selectedNoteBody;
+        set => this.RaiseAndSetIfChanged(ref _selectedNoteBody, value);
+    }
 
-    public NoteCollectionViewModel NoteCollection { get; set; } = new();
+    // Constructor
+    public MainWindowViewModel()
+    {
+        // Simulate loading notes (in a real scenario, load from a data source)
+        NoteTitles = new ObservableCollection<string>
+        {
+            "Note 1",
+            "Note 2",
+            "Note 3"
+        };
+
+        // Set a default selection (first note)
+        if (NoteTitles.Count > 0)
+        {
+            SelectedNoteTitle = NoteTitles[0];
+        }
+    }
 }
